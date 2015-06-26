246d555b214987865229f7f8b7499c3d0e3aeae9
James Steinigner <jesteininger@gmail.com>
2015-06-26 19:10:22 +0000

    nitrous switch

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -13,3 +13,9 @@ class Answer < ActiveRecord::Base
     simple_format
   end
 end
+
+AutoHtml.add_filter(:image) do |text|
+  text.gsub(/http:\/\/.+\.(jpg|jpeg|bmp|gif|png)(\?\S+)?/i) do |match|
+    %|<img src="#{match}" alt=""/>|
+  end
+end
